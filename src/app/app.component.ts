37155041d7a7debb0485bd46bc477b12e33bd0b7
Alexandr <valverin95@gmail.com>
2021-09-18 21:37:38 +0300

    feat: [toggle-buttons] обновлен демонстрационный шаблон

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ToggleButtonsEvent } from "../../projects/am-bulba/src/lib/toggle-buttons/toggle-buttons-group.directive";
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,25 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class AppComponent {
   title = 'am-bulbalar';
+
+  buttons = [
+    {
+      title: 'left',
+      checked: false,
+    },
+    {
+      title: 'center',
+      checked: false,
+    },
+    {
+      title: 'right',
+      checked: true,
+    },
+  ]
+
+  value: ToggleButtonsEvent | undefined;
+
+  onChange(event: ToggleButtonsEvent) {
+    this.value = event;
+  }
 }

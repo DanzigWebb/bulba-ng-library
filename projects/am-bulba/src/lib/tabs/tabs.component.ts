087fa761644e50de2aad4aaf9d3876e6709a7c77
Alexandr <valverin95@gmail.com>
2021-09-27 21:47:51 +0300

    feat: [tabs] отображение контента таба

--- projects/am-bulba/src/lib/tabs/tabs.component.ts
+++ projects/am-bulba/src/lib/tabs/tabs.component.ts
@@ -1,20 +1,35 @@
-import { Component, ContentChildren, forwardRef, OnInit, QueryList } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, Input, OnInit, QueryList } from '@angular/core';
 import { TabComponent } from "./tab/tab.component";
 
 @Component({
   selector: 'am-tabs-group',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.css']
+  styleUrls: ['./tabs.component.css'],
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent implements OnInit, AfterContentInit {
 
-  @ContentChildren(forwardRef(() => TabComponent), {
-    descendants: true,
-  }) public toggleButtons: QueryList<TabComponent> | undefined;
+  @Input() isBoxed = false;
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
+    if (this.tabs) {
+      const activeTabs = this.tabs.filter((tab) => tab.active) || [];
+
+      if (activeTabs.length === 0) {
+        this.selectTab(this.tabs.first);
+      }
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.toArray().forEach(tab => tab.active = false);
+    tab.active = true;
   }
 
 }

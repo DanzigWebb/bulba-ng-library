ad6178ae004e213bf56ddb5e7d533ad03e16252c
Alexandr <valverin95@gmail.com>
2021-09-29 21:37:05 +0300

    feat: [dialogs] added example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { ToggleButtonsEvent } from "../../projects/am-bulba/src/lib/toggle-buttons/toggle-buttons-group.directive";
 import { FormControl } from "@angular/forms";
 import { TabsPositionType, TabsSizeType, TabsViewType } from "../../projects/am-bulba/src/lib/tabs/tabs.type";
+import { DialogService } from "../../projects/am-bulba/src/lib/dialog/dialog.service";
+import { Dialog } from "../../projects/am-bulba/src/lib/dialog/models/dialog.model";
 
 @Component({
   selector: 'app-root',
@@ -40,9 +42,50 @@ export class AppComponent {
     console.log('onChange event:', event);
   }
 
-  constructor() {
+  constructor(
+    private dialog: DialogService,
+  ) {
     this.control.valueChanges.subscribe(data => {
       console.log('valueChanges subscribe:', data);
     });
+  }
+
+  open() {
+    const text = 'First Dialog.';
+    this.dialog.open(ExampleDialog, text);
+  }
+}
+
+@Component({
+  template: `
+      <div class="modal-content">
+          <div class="card">
+              <div class="card-content">
+                  {{title}}
+              </div>
+              <button class="button" (click)="open()">Open dialog</button>
+          </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" (click)="close()"></button>
+  `,
+})
+export class ExampleDialog extends Dialog {
+  title = '';
+
+  constructor(
+    private dialog: DialogService,
+  ) {super();}
+
+  onInjectInputs(title: string): void {
+    this.title = title;
+  }
+
+  close(output?: any) {
+    super.close(output);
+  }
+
+  open() {
+    const text = 'Second Dialog.';
+    this.dialog.open(ExampleDialog, text);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent, ExampleDialog } from './app.component';
 import { ToggleButtonsModule } from "../../projects/am-bulba/src/lib/toggle-buttons/toggle-buttons.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NavbarModule } from "../../projects/am-bulba/src/lib/navbar/navbar.module";
 import { TabsModule } from "../../projects/am-bulba/src/lib/tabs/tabs.module";
 import { PanelModule } from "../../projects/am-bulba/src/lib/panel/panel.module";
+import { DialogModule } from "../../projects/am-bulba/src/lib/dialog/dialog.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleDialog,
   ],
   imports: [
     BrowserModule,
@@ -21,6 +23,7 @@ import { PanelModule } from "../../projects/am-bulba/src/lib/panel/panel.module"
     NavbarModule,
     TabsModule,
     PanelModule,
+    DialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

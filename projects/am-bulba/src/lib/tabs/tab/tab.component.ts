a38253e278ee59d2c3229ecb380501ebad24069f
Alexandr <valverin95@gmail.com>
2021-10-05 22:03:27 +0300

    feat: [tabs] Added animations

--- projects/am-bulba/src/lib/tabs/tab/tab.component.ts
+++ projects/am-bulba/src/lib/tabs/tab/tab.component.ts
@@ -1,10 +1,27 @@
 import { Component, ContentChild, Input, OnInit } from '@angular/core';
 import { TabLabelComponent } from "../tab-label/tab-label.component";
+import { animate, style, transition, trigger } from "@angular/animations";
+
+const animationSlide = [
+    trigger('slideLeft', [
+    transition(':enter', [
+      style({
+        opacity: 0,
+        transform: 'translateX(-100%)',
+      }),
+      animate('350ms cubic-bezier(0, 0, 0.2, 1)', style({
+        opacity: 1,
+        transform: 'translateX(0)',
+      })),
+    ]),
+  ]),
+];
 
 @Component({
   selector: 'am-tab',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.css']
+  styleUrls: ['./tab.component.css'],
+  animations: animationSlide
 })
 export class TabComponent implements OnInit {
 

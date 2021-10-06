4e3e082538cd496a7b6aa1eb3c0cfe05c16e8465
Alexandr <valverin95@gmail.com>
2021-10-06 21:08:17 +0300

    feat: [menu] release mvp component

--- projects/am-bulba/src/lib/menu/menu-panel/menu-panel.component.ts
+++ projects/am-bulba/src/lib/menu/menu-panel/menu-panel.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: 'am-menu-panel',
   exportAs: 'amMenu',
   templateUrl: './menu-panel.component.html',
-  styleUrls: ['./menu-panel.component.css']
+  styleUrls: ['./menu-panel.component.css'],
+  animations: [
+    trigger('slide', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(-60px)',
+          opacity: 0,
+        }),
+        animate('150ms cubic-bezier(0, 0, 0.2, 1)', style({
+          transform: 'translateY(0)',
+          opacity: 1,
+        })),
+      ]),
+    ]),
+  ]
 })
 export class MenuPanelComponent implements OnInit {
+
+  @ViewChild(TemplateRef) panelContent: TemplateRef<any> | undefined;
+
+  @Output() onClose = new EventEmitter();
+
+
+  get instance() {
+    return this;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  close() {
+    this.onClose.emit();
+  }
 }
--- projects/am-bulba/src/lib/menu/menu-trigger.directive.ts
+++ projects/am-bulba/src/lib/menu/menu-trigger.directive.ts
@@ -1,17 +1,83 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  EmbeddedViewRef,
+  Inject,
+  Input,
+  NgZone,
+  OnInit,
+  ViewContainerRef,
+} from '@angular/core';
 import { MenuPanelComponent } from "./menu-panel/menu-panel.component";
+import { createPopper, Instance } from "@popperjs/core";
+import { DOCUMENT } from "@angular/common";
+
 
 @Directive({
-  selector: '[amMenuTrigger]'
+  selector: '[amMenuTrigger]',
+  exportAs: 'amMenuTrigger',
+  host: {
+    '(click)': 'show()',
+  },
 })
 export class MenuTriggerDirective implements OnInit {
+  private _panelContent!: MenuPanelComponent;
 
-  @Input() menu!: MenuPanelComponent;
+  private view: EmbeddedViewRef<any> | null = null;
+  private panelRef: HTMLElement | null = null;
+  private popperRef: Instance | null = null;
 
-  constructor() {
+  private isOpen = false;
+
+  @Input() set amMenuTrigger(panel: MenuPanelComponent) {
+    this._panelContent = panel;
+  }
+
+  get panelContent() { return this._panelContent; }
+
+  constructor(
+    @Inject(DOCUMENT) private doc: Document,
+    private vcr: ViewContainerRef,
+    private el: ElementRef,
+    private zone: NgZone,
+  ) {
   }
 
   ngOnInit() {
-    console.log(this.menu);
+    this.panelContent.onClose.subscribe(() => {
+      this.close()
+    })
+  }
+
+  show() {
+    if (this.isOpen) {
+      return;
+    }
+    const panelTpl = this.panelContent.panelContent;
+    if (panelTpl) {
+      this.view = this.vcr.createEmbeddedView(panelTpl);
+      this.panelRef = <HTMLElement>this.view.rootNodes[0];
+      const dropdown = <HTMLElement>this.panelRef.querySelector('.am-menu-panel-box');
+
+      this.doc.body.appendChild(this.panelRef);
+
+      if (this.panelRef && dropdown) {
+        this.createPopper(this.el.nativeElement, dropdown);
+        this.isOpen = true;
+      }
+    }
+  }
+
+  close() {
+    this.view?.destroy();
+    this.panelRef?.remove();
+    this.popperRef?.destroy();
+    this.isOpen = false;
+  }
+
+  private createPopper(trigger: HTMLElement, panel: HTMLElement) {
+    this.zone.runOutsideAngular(() => {
+      this.popperRef = this.popperRef = createPopper(trigger, panel);
+    });
   }
 }
